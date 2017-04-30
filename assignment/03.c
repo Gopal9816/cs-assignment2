af9e21877b55c51ce1c1c6fe82a76bceb8134422
user9747 <padinju@gmail.com>
2017-04-30 13:04:39 +0530

    IsUpperTriangle

---
+++ assignment/03.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int IsUpper(int a[50][50],int n)
+{
+	for(int i=0;i<n;i++)
+	for(int j=0;j<n;j++)
+		if(i>j&&a[i][j]==0)
+			return (1);
+}
+
+
+int main()
+{
+int a[50][50],n,s=0;
+printf("enter the size of matrix");
+scanf("%d",&n);
+printf("enter the matrix"); 
+for(int i=0;i<n;i++)
+for(int j=0;j<n;j++)
+scanf("%d",&a[i][j]);
+
+s=IsUpper(a,n);
+printf("\n%d\n",s);
+}
+
