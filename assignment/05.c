06bdedcb95c2d0e621461ec3ef7e869ec99fbbf8
madmax98 <majidmahmood98@gmail.com>
2017-04-30 14:09:10 +0530

    delete substring

---
+++ assignment/05.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	char str[50],l[10],m,k,i,j=0,pos;
+	printf("Enter the line of text: ");
+	scanf("%[^\n]s",str);
+	printf("Enter substring to be deleted: ");
+	scanf("%s",l);
+	for(i=0;str[i]!='\0';++i)
+		if(str[i]==l[j])
+			j++;
+		else if(l[j]=='\0')
+			break;
+		else
+			j=0;
+	if(j==0)
+		printf("Substring not found");
+	else
+	{
+		for(k=i-j;str[i]!='\0';)
+			str[k++]=str[i++];
+		str[k]='\0';
+	}
+	printf("%s",str);
+
+}
