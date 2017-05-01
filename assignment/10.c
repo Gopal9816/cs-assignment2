0ccfc13e39af6425ee814d724a71a22237ca81a6
Mohd-Bilal <bilaljaleel123@gmail.com>
2017-05-01 20:24:23 +0530

    modified

--- assignment/10.c
+++ assignment/10.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-char m[100];
-int i=0;
-char stringrev(char a[100],int n)
-{return a[n-i]
 
-m[i++]=stringrev(a[n-i],i);
+void stringrev(char *a)
+{
+if(*a!='\0')
+{
+	stringrev(a+1);
+	printf("%c",*a);
+}
 
 
 }
@@ -12,14 +14,11 @@ m[i++]=stringrev(a[n-i],i);
 
 void main()
 {
-char *a,s[100];
-int n=0;
+char s[100];
+
 printf("Enter the string ");
 scanf("%s",s);
-for(int i=0;s[i]!='\0';i++)
-n++;
 
-a=stringrev(s,n);
-printf("%s",a);
+stringrev(s);
 
 }
